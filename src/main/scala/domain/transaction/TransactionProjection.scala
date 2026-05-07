ae2e53798bfac270a038c437cfb7f502e9926d30
bjolidon <bastien.jolidon@epfl.ch>
2026-05-07 15:43:02 +0200

    Solution step 3: implement all projections

--- src/main/scala/domain/transaction/TransactionProjection.scala
+++ src/main/scala/domain/transaction/TransactionProjection.scala
@@ -15,16 +15,94 @@ final class TransactionProjection private (
 
   override def name: String = "TransactionProjection"
 
-  override def filter: Set[EventTypeName] = ???
+  override def filter: Set[EventTypeName] =
+    Set(
+      EventTypeName.of[Deposited],
+      EventTypeName.of[Withdrawn],
+      EventTypeName.of[Transferred]
+    )
 
   override def resolveKeys(
       event: EventEnvelope[BankEvent]
-  ): List[UUID] = ???
+  ): List[UUID] = event.payload match
+    case Deposited(transactionId, accountId, amount, timestamp) =>
+      List(transactionId)
+    case Withdrawn(transactionId, accountId, amount, timestamp) =>
+      List(transactionId)
+    case Transferred(
+          transactionId,
+          debitAccountId,
+          creditAccountId,
+          amount,
+          timestamp
+        ) =>
+      List(transactionId)
+    case _ => Nil
 
   override def handle(
       state: Option[Transaction],
       event: EventEnvelope[BankEvent]
-  ): IO[Option[Transaction]] = ???
+  ): IO[Option[Transaction]] = (state, event.payload) match
+    case (
+          None,
+          Deposited(transactionId, accountId, amount, timestamp)
+        ) =>
+      IO.pure(
+        Some(
+          Transaction(
+            transactionId = transactionId,
+            debitAccountId = None,
+            creditAccountId = Some(accountId),
+            transactionType = TransactionType.Deposit,
+            amount = amount,
+            timestamp = timestamp
+          )
+        )
+      )
+    case (
+          None,
+          Withdrawn(transactionId, accountId, amount, timestamp)
+        ) =>
+      IO.pure(
+        Some(
+          Transaction(
+            transactionId = transactionId,
+            debitAccountId = Some(accountId),
+            creditAccountId = None,
+            transactionType = TransactionType.Withdrawal,
+            amount = amount,
+            timestamp = timestamp
+          )
+        )
+      )
+    case (
+          None,
+          Transferred(
+            transactionId,
+            debitAccountId,
+            creditAccountId,
+            amount,
+            timestamp
+          )
+        ) =>
+      IO.pure(
+        Some(
+          Transaction(
+            transactionId = transactionId,
+            debitAccountId = Some(debitAccountId),
+            creditAccountId = Some(creditAccountId),
+            transactionType = TransactionType.Transfer,
+            amount = amount,
+            timestamp = timestamp
+          )
+        )
+      )
+    case _ =>
+      IO.raiseError(
+        new RuntimeException(
+          s"Failed to apply event: ${event.payload} to state: $state"
+        )
+      )
 
 }
 
